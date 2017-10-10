5afbe6447900cdad440bc9c704c1df586514195f
Daniel Rodosky <djrodosky@gmail.com>
2017-10-10 10:05:37 -0600

    Initial Commit

---
+++ PokemonHybrid/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PokemonHybrid
+//
+//  Created by Daniel Rodosky on 10/10/17.
+//  Copyright © 2017 Daniel Rodosky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PokemonHybrid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PokemonHybrid
+//
+//  Created by Daniel Rodosky on 10/10/17.
+//  Copyright © 2017 Daniel Rodosky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
